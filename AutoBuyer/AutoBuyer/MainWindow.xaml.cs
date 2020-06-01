7bd3e5a577ace5e474432fbedc3a2cd35673fcd7
gulei <gulei@rangingsec.com>
2020-06-01 22:33:00 +0800

    w

--- AutoBuyer/AutoBuyer/MainWindow.xaml.cs
+++ AutoBuyer/AutoBuyer/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +22,51 @@ namespace AutoBuyer
     /// </summary>
     public partial class MainWindow : Window
     {
+        PhoneBuyer buyer;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            buyer = new PhoneBuyer();
+            buyer.Login();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            buyer.ToNavigate();
+            buyer.ToClick();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            buyer.Close();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            buyer.ToNavigate();
+            for (int i = 0; i < 10000000; i++)
+            {
+                buyer.ToClick();
+                buyer.sss2();
+            }
+        }
+
+        private void test(object source, ElapsedEventArgs e)
+        {
+            //if (DateTime.Now.Hour == 14 && DateTime.Now.Minute == 59 && DateTime.Now.Second == 57)
+            //{
+
+                
+                //buyer.ToClick();
+                //buyer.sss2();
+                //buyer.ToClick();
+                //buyer.sss2();
+            // }
         }
     }
 }
---
+++ AutoBuyer/AutoBuyer/PhoneBuyer.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace AutoBuyer
+{
+    public class PhoneBuyer
+    {
+        IWebDriver driver;
+        public void Login()
+        {
+
+            var service = EdgeDriverService.CreateDefaultService(@"C:\Users\Stone\Documents\GitHub\AutoBuyer\AutoBuyer\AutoBuyer\bin\Debug\netcoreapp3.1\DriversApplication", "msedgedriver.exe");
+
+            string s = AppDomain.CurrentDomain.BaseDirectory + @"DriversApplication\msedgedriver.exe";
+            driver = new EdgeDriver(service);
+            driver.Navigate().GoToUrl(@"https://passport.jd.com/new/login.aspx");
+            driver.FindElement(By.LinkText("账户登录")).Click();
+            driver.FindElement(By.Id("loginname")).SendKeys("love13142nan");
+            driver.FindElement(By.Id("nloginpwd")).SendKeys("stone13142nan%");
+        }
+
+        public void ToNavigate()
+        {
+            driver.Navigate().GoToUrl(@"https://pro.jd.com/mall/active/4MmNbTkve7YDv3cqfN65jyU54rSB/index.html8");
+
+        }
+
+        public void ToClick()
+        {
+            driver.FindElement(By.XPath(@"//*[@id='J_babelOptPage']/div/div[5]/div/div/a")).Click();
+        }
+
+        public void sss2()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+            IWebElement webElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("close-button")));
+            if (webElement.Displayed == true )
+            {
+                driver.FindElement(By.ClassName("close-button")).Click();
+            }
+
+        }
+
+        public void Close()
+        {
+            driver.Close();
+        }
+    }
+}
